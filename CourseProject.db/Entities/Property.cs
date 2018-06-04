c6f9f39e0d65cf9ab6b11be2b5ed84d1b0cdafbe
dyakovg <34554774+dyakovg@users.noreply.github.com>
2018-06-04 22:11:36 +0300

    added model

--- CourseProject.db/Entities/Property.cs
+++ CourseProject.db/Entities/Property.cs
@@ -3,8 +3,14 @@
 namespace CourseProject.db.Entities
 {
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Property : BaseEntity
     {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int PropertyId { get; set; }
+
         [Required, StringLength(20)]
         public string Type { get; set; }
 
--- CourseProject/Models/PropertiesViewModel.cs
+++ CourseProject/Models/PropertiesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using CourseProject.db.Entities;
@@ -8,6 +10,39 @@ namespace CourseProject.Models
 {
     public class PropertiesViewModel
     {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int PropertyId { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public int Bedrooms { get; set; }
+
+        [Required]
+        public int Rooms { get; set; }
+
+        [Required]
+        public int Area { get; set; }
+
+        [Required]
+        public int Floor { get; set; }
+
+        [Required]
+        public string ResidentialDistrict { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public string Extras { get; set; }
+
         public List<Property> AllProperties { get; set; }
 
         public List<Property> GetProperties()
