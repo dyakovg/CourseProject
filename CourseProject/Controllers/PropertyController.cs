42a43e0bc5928c3b200f613439ee394f566aedf2
dyakovg <34554774+dyakovg@users.noreply.github.com>
2018-06-04 20:56:36 +0300

    adding model

--- CourseProject/Controllers/PropertyController.cs
+++ CourseProject/Controllers/PropertyController.cs
@@ -3,6 +3,7 @@ using CourseProject.DataAccess.Repositories;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using CourseProject.DataAccess;
+using CourseProject.Models;
 
 namespace CourseProject.Controllers
 {
@@ -20,7 +21,20 @@ namespace CourseProject.Controllers
 
         public ActionResult Index()
         {
-            List<Property> model = repo.GetAll();
+            List<Property> properties = repo.GetAll();
+            List<PropertiesViewModel> model = new List<PropertiesViewModel>();
+
+            foreach (Property p in properties)
+            {
+                PropertiesViewModel pm = new PropertiesViewModel()
+                {
+                    AllProperties = p.Type
+                };
+
+                model.Add(pm);
+            }
+           
+           
 
             return View(model);
         }
---
+++ CourseProject/Models/PropertiesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CourseProject.db.Entities;
+
+namespace CourseProject.Models
+{
+    public class PropertiesViewModel
+    {
+        public List<Property> AllProperties { get; set; }
+
+        public List<Property> GetProperties()
+        {
+            return AllProperties;
+        }
+    }
+}
